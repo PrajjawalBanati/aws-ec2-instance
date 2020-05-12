3e839f3c0d910fc1bc633ebee0311f3d2af03365
Prajjawal banati <500060722@stu.upes.ac.in>
2020-05-12 18:57:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,6 @@
 variable "instance_image" {
     type=string
 }
-variable "public_key_file_path" {
-    type=string
-}
-variable "private_key_file_path" {
-    type=string
-}
 variable "cluster-count" {
     type=number
 }
